33ac487dd28cf217a6b21abf44906ff20c85f37c
Kevin Demetro <albinodemetrok@gmail.com>
2025-06-24 20:03:52 -0300

    Cria componentes usando styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+        <Button>Check Out</Button>
+        <Input />
+        <Input />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
