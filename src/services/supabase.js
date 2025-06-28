9481160a3f3a388b6d2e56b2ecb7fd26502712fe
Kevin Demetro <albinodemetrok@gmail.com>
2025-06-28 13:09:36 -0300

    upload de imagem

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://dyiindtjihlznlwakrko.supabase.co";
+export const supabaseUrl = "https://dyiindtjihlznlwakrko.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5aWluZHRqaWhsem5sd2FrcmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4ODE1MjksImV4cCI6MjA2NjQ1NzUyOX0.skbrAclNgh39AAJEjQlL4aNvzkyQnkERnbhTGjeRYOA";
 const supabase = createClient(supabaseUrl, supabaseKey);
