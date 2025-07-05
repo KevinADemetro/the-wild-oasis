c7a6f7f2a6cd6f561026b961e5135bd674f6a8b5
Kevin Demetro <albinodemetrok@gmail.com>
2025-07-04 23:02:43 -0300

    Utiliza padrão render prop para mostrar a lista de cabinRow

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -86,11 +86,15 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Row = Row;
-Table.body = Body;
+Table.Body = Body;
 Table.Footer = Footer;
 
 export default Table;
