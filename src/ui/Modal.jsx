5656ea208f1daeda2678d5637a635816226021e9
Kevin Demetro <albinodemetrok@gmail.com>
2025-07-04 21:32:06 -0300

    Utiliza modal para editar cabin e confirmar delete

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,13 +1,6 @@
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
-import {
-  cloneElement,
-  createContext,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { cloneElement, createContext, useContext, useState } from "react";
 import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
